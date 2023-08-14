50299a243588fc6e46ba020d53c5952d0ee0073e
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-08-14 18:04:08 +0100

    Create linkedlist.c

---
+++ linkedlist.c
@@ -0,0 +1,58 @@
+/*
+ * File: linkedlist.c
+ */
+
+#include "shell.h"
+
+alias_t *add_alias_end(alias_t **head, char *name, char *value);
+void free_alias_list(alias_t *head);
+list_t *add_node_end(list_t **head, char *dir);
+void free_list(list_t *head);
+
+/**
+ * add_alias_end - Adds a node to the end of a alias_t linked list.
+ * @head: A pointer to the head of the list_t list.
+ * @name: The name of the new alias to be added.
+ * @value: The value of the new alias to be added.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
+ */
+alias_t *add_alias_end(alias_t **head, char *name, char *value)
+{
+/* implementation */
+}
+
+/**
+ * add_node_end - Adds a node to the end of a list_t linked list.
+ * @head: A pointer to the head of the list_t list.
+ * @dir: The directory path for the new node to contain.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
+ */
+list_t *add_node_end(list_t **head, char *dir)
+{
+/* implementation */
+}
+
+/**
+ * free_alias_list - Frees a alias_t linked list.
+ * @head: THe head of the alias_t list.
+ */
+void free_alias_list(alias_t *head)
+{
+/* implementation */
+}
+
+/**
+ * free_list - Frees a list_t linked list.
+ * @head: The head of the list_t list.
+ */
+void free_list(list_t *head)
+{
+/* implementation */
+}
+
+
+
