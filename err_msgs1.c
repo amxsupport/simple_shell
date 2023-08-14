52b4a220ec7da658a33d7d1db0501d66f3d25747
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-08-14 19:15:20 +0100

    Update err_msgs1.c implementation of error_1 and error_2_exit functions
    these functions Creates an error message for shellby_exit errors.
    and creates an error message for shellby_alias errors.

--- err_msgs1.c
+++ err_msgs1.c
@@ -53,7 +53,19 @@ char *error_env(char **args)
  */
 char *error_1(char **args)
 {
-/* implementation */
+	char *error;
+	int len;
+
+	len = _strlen(name) + _strlen(args[0]) + 13;
+	error = malloc(sizeof(char) * (len + 1));
+	if (!error)
+		return (NULL);
+
+	_strcpy(error, "alias: ");
+	_strcat(error, args[0]);
+	_strcat(error, " not found\n");
+
+	return (error);
 }
 
 /**
@@ -64,7 +76,30 @@ char *error_1(char **args)
  */
 char *error_2_exit(char **args)
 {
-/* implementation */
+	char *error, *hist_str;
+	int len;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+
+	len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 27;
+	error = malloc(sizeof(char) * (len + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+
+	_strcpy(error, name);
+	_strcat(error, ": ");
+	_strcat(error, hist_str);
+	_strcat(error, ": exit: Illegal number: ");
+	_strcat(error, args[0]);
+	_strcat(error, "\n");
+
+	free(hist_str);
+	return (error);
 }
 
 /**
