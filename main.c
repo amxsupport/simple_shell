2ebe55082719ef4e0bba113b2887007224b56ae9
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-14 01:14:01 +0100

    Update main.c

--- main.c
+++ main.c
@@ -28,3 +28,15 @@ int execute(char **args, char **front)
 {
   /* implementing execute */ 
 }
+
+/**
+ * main - Runs a simple UNIX command interpreter.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: The return value of the last executed command.
+ */
+int main(int argc, char *argv[])
+{
+ /* implementing main */ 
+}
