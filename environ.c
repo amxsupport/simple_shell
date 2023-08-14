1f786ce6b09ecf3e229877da43da7b762c10a860
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-14 13:36:52 +0100

    Create environ.c

---
+++ environ.c
@@ -0,0 +1,40 @@
+/*
+ * File: environ.c
+ */
+
+#include "shell.h"
+
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
+
+/**
+ * _copyenv - Creates a copy of the environment.
+ *
+ * Return: If an error occurs - NULL.
+ *         O/w - a double pointer to the new copy.
+ */
+char **_copyenv(void)
+{
+/* implementing */
+}
+
+/**
+ * free_env - Frees the the environment copy.
+ */
+void free_env(void)
+{
+/* implementing */
+}
+
+/**
+ * _getenv - Gets an environmental variable from the PATH.
+ * @var: The name of the environmental variable to get.
+ *
+ * Return: If the environmental variable does not exist - NULL.
+ *         Otherwise - a pointer to the environmental variable.
+ */
+char **_getenv(const char *var)
+{
+/* implementing */
+}
