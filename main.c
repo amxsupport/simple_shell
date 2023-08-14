1bee092229ce1390dccc31f435a64bfc08e0978a
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-14 01:11:57 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+/*
+ * File: main.c
+ *
+ */
+
+#include "shell.h"
+void sig_handler(int sig);
+int execute(char **args, char **front);
+
+/**
+ * sig_handler - Prints a new prompt upon a signal.
+ * @sig: The signal.
+ */
+void sig_handler(int sig)
+{
+ /* implementing sig_handler*/ 
+}
+
+/**
+ * execute - Executes a command in a child process.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: If an error occurs - a corresponding error code.
+ *         O/w - The exit value of the last executed command.
+ */
+int execute(char **args, char **front)
+{
+  /* implementing execute */ 
+}
