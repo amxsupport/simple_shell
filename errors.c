083af32c287b9fef3f02f791330cb9a431cdeae9
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-08-14 18:40:29 +0100

    Create errors.c and implement num_len function

---
+++ errors.c
@@ -0,0 +1,61 @@
+/*
+ * File: errors.c
+ */
+
+#include "shell.h"
+
+int num_len(int num);
+char *_itoa(int num);
+int create_error(char **args, int err);
+
+/**
+ * num_len - Counts the digit length of a number.
+ * @num: The number to measure.
+ *
+ * Return: The digit length.
+ */
+int num_len(int num)
+{
+	unsigned int num1;
+	int len = 1;
+
+	if (num < 0)
+	{
+		len++;
+		num1 = num * -1;
+	}
+	else
+	{
+		num1 = num;
+	}
+	while (num1 > 9)
+	{
+		len++;
+		num1 /= 10;
+	}
+
+	return (len);
+}
+
+/**
+ * _itoa - Converts an integer to a string.
+ * @num: The integer.
+ *
+ * Return: The converted string.
+ */
+char *_itoa(int num)
+{
+/* implementation */
+}
+
+/**
+ * create_error - Writes a custom error message to stderr.
+ * @args: An array of arguments.
+ * @err: The error value.
+ *
+ * Return: The error value.
+ */
+int create_error(char **args, int err)
+{
+/* implementation */
+}
