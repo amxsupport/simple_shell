c36751aece2b4285ea5fcbdc1f8c9167572684df
amxsupport <itmasterdesigne@gmail.com>
2023-08-16 11:32:35 +0000

    create locate.c and 3 functions for implementation

---
+++ locate.c
@@ -0,0 +1,47 @@
+/*
+ * File: locate.c
+ *
+ */
+
+#include "shell.h"
+
+char *fill_path_dir(char *path);
+list_t *get_path_dir(char *path);
+
+/**
+ * get_location - Locates a command in the PATH.
+ * @command: The command to locate.
+ *
+ * Return: If an error occurs or the command cannot be located - NULL.
+ *         Otherwise - the full pathname of the command.
+ */
+char *get_location(char *command)
+{
+/* implementation */
+}
+
+/**
+ * fill_path_dir - Copies path but also replaces leading/sandwiched/trailing
+ *		   colons (:) with current working directory.
+ * @path: The colon-separated list of directories.
+ *
+ * Return: A copy of path with any leading/sandwiched/trailing colons replaced
+ *	   with the current working directory.
+ */
+char *fill_path_dir(char *path)
+{
+	/* implementation */
+}
+
+/**
+ * get_path_dir - Tokenizes a colon-separated list of
+ *                directories into a list_s linked list.
+ * @path: The colon-separated list of directories.
+ *
+ * Return: A pointer to the initialized linked list.
+ */
+list_t *get_path_dir(char *path)
+{
+	/* implementation */
+}
+
