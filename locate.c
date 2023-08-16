be91e62050e95217c5f119ad4503c075194a763f
amxsupport <itmasterdesigne@gmail.com>
2023-08-16 12:29:53 +0000

    update locate.c get_path_dir - Tokenizes a colon-separated list of directories into a list_s linked list.

--- locate.c
+++ locate.c
@@ -117,6 +117,30 @@ char *fill_path_dir(char *path)
  */
 list_t *get_path_dir(char *path)
 {
-	/* implementation */
+	int index;
+	char **dirs, *path_copy;
+	list_t *head = NULL;
+
+	path_copy = fill_path_dir(path);
+	if (!path_copy)
+		return (NULL);
+	dirs = _strtok(path_copy, ":");
+	free(path_copy);
+	if (!dirs)
+		return (NULL);
+
+	for (index = 0; dirs[index]; index++)
+	{
+		if (add_node_end(&head, dirs[index]) == NULL)
+		{
+			free_list(head);
+			free(dirs);
+			return (NULL);
+		}
+	}
+
+	free(dirs);
+
+	return (head);
 }
 
