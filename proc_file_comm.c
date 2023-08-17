43a342fe53862278f3defbb5be5a48791c4ba207
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-17 13:19:51 +0100

    Update proc_file_comm.c implement cant_open function
    cant_open - If the file doesn't exist or lacks proper permissions, print a cant open error.

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -13,7 +13,32 @@ int proc_file_commands(char *file_path, int *exe_ret);
 
 int cant_open(char *file_path)
 {
-/* implementaion */
+	char *error, *hist_str;
+	int len;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (127);
+
+	len = _strlen(name) + _strlen(hist_str) + _strlen(file_path) + 16;
+	error = malloc(sizeof(char) * (len + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (127);
+	}
+
+	_strcpy(error, name);
+	_strcat(error, ": ");
+	_strcat(error, hist_str);
+	_strcat(error, ": Can't open ");
+	_strcat(error, file_path);
+	_strcat(error, "\n");
+
+	free(hist_str);
+	write(STDERR_FILENO, error, len);
+	free(error);
+	return (127);
 }
 
 /**
