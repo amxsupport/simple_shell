030ceb44690b7d2c197a2877bbe2df60e6cc8028
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2023-08-17 13:58:18 +0100

    Update builtin.c update shellby_exit function

--- builtin.c
+++ builtin.c
@@ -51,30 +51,35 @@ int (*get_builtin(char *command))(char **args)
  */
 int shellby_exit(char **args)
 {
-	int i = 0, sign = 1;
-	unsigned int num = 0;
+	int i, len_of_int = 10;
+	unsigned int num = 0, max = 1 << (sizeof(int) * 8 - 1);
 
 	if (args[0])
 	{
-		if (args[0][i] == '-')
-			sign = -1;
+		if (args[0][0] == '+')
+		{
+			i = 1;
+			len_of_int++;
+		}
 		for (; args[0][i]; i++)
 		{
-			if (args[0][i] == '-')
-				sign *= -1;
-
-			if (args[0][i] >= '0' && args[0][i] <= '9')
+			if (i <= len_of_int && args[0][i] >= '0' && args[0][i] <= '9')
 				num = (num * 10) + (args[0][i] - '0');
 			else
-				return (2);
+				return (create_error(--args, 2));
 		}
 	}
+	else
+	{
+		return (-3);
+	}
+	if (num > max - 1)
+		return (create_error(--args, 2));
 	args -= 1;
-	for (i = 0; args[i]; i++)
-		free(args[i]);
-	free(args);
+	free_args(args, front);
 	free_env();
-	exit(num * sign);
+	free_alias_list(aliases);
+	exit(num);
 }
 
 /**
