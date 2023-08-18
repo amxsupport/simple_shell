5cf791572c67a6313ba13115a93114a571864e84
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-18 02:00:22 +0100

    Create builtins_help_1.c

---
+++ builtins_help_1.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+void help_all(void);
+void help_alias(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
+
+/**
+ * help_all - Displays all possible builtin shellby commands.
+ */
+void help_all(void)
+{
+/* implementation */
+}
+
+/**
+ * help_alias - Displays information on the shellby builtin command 'alias'.
+ */
+void help_alias(void)
+{
+  /* implementation */
+}
+
+/**
+ * help_cd - Displays information on the shellby builtin command 'cd'.
+ */
+void help_cd(void)
+{
+  /* implementation */
+}
+
+/**
+ * help_exit - Displays information on the shellby builtin command 'exit'.
+ */
+void help_exit(void)
+{
+    /* implementation */
+}
+
+/**
+ * help_help - Displays information on the shellby builtin command 'help'.
+ */
+void help_help(void)
+{
+    /* implementation */
+}
