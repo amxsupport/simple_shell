8a2547377bb55a0a55e6064b9354e1dce399036e
amxsupport <itmasterdesigne@gmail.com>
2023-08-18 11:11:27 +0000

    update builtins_help_2 set help_unsetenv function

--- builtins_help_2.c
+++ builtins_help_2.c
@@ -37,5 +37,11 @@ void help_setenv(void)
  */
 void help_unsetenv(void)
 {
-	/* implementation */
+	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environmental variable.\n\n\tUpon failure, prints a ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
