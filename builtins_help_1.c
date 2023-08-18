def51d683ae122514e6ec6d8cd614d09163bd4fe
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-18 02:01:40 +0100

    Update builtins_help_1.c iplementation of help_all function

--- builtins_help_1.c
+++ builtins_help_1.c
@@ -11,7 +11,21 @@ void help_help(void);
  */
 void help_all(void)
 {
-/* implementation */
+  char *msg = "Shellby\nThese shell commands are defined internally.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "Type 'help' to see this list.\nType 'help name' to find ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "out more about the function 'name'.\n\n  alias   \t";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "alias [NAME[='VALUE'] ...]\n  cd    \tcd   ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "[DIRECTORY]\n  exit    \texit [STATUS]\n  env     \tenv";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n  setenv  \tsetenv [VARIABLE] [VALUE]\n  unsetenv\t";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "unsetenv [VARIABLE]\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
