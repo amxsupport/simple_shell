bbdec123ea84d237948638845c96d1b8eb295ddb
amxsupport <itmasterdesigne@gmail.com>
2023-08-18 10:57:29 +0000

    create builtins_help_2.c

---
+++ builtins_help_2.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
+
+/**
+ * help_env - Displays information on the shellby builtin command 'env'.
+ */
+void help_env(void)
+{
+	/* implementation */
+}
+
+
+/**
+ * help_setenv - Displays information on the shellby builtin command 'setenv'.
+ */
+void help_setenv(void)
+{
+	/* implementation */
+}
+
+
+/**
+ * help_unsetenv - Displays information on the shellby builtin command
+ * 'unsetenv'.
+ */
+void help_unsetenv(void)
+{
+	/* implementation */
+}
