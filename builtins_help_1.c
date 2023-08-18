d4e43875f87512bae6780f391036df969c8fb510
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-18 02:05:28 +0100

    Update builtins_help_1.c implement help_exit function

--- builtins_help_1.c
+++ builtins_help_1.c
@@ -75,7 +75,15 @@ void help_cd(void)
  */
 void help_exit(void)
 {
-    /* implementation */
+	char *msg = "exit: exit [STATUS]\n\tExits the shell.\n\n\tThe ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "STATUS argument is the integer used to exit the shell.";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " If no argument is given, the command is interpreted as";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " exit 0.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
