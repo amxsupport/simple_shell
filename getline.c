a2d1aa6342997a648070c89f9fd948a2d165a708
amxsupport <itmasterdesigne@gmail.com>
2023-08-19 01:04:40 +0000

    update getline.c implementation of _getline function

--- getline.c
+++ getline.c
@@ -102,6 +102,45 @@ void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
  */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
-	/* iplementation */
+	static ssize_t input;
+	ssize_t ret;
+	char c = 'x', *buffer;
+	int r;
+
+	if (input == 0)
+		fflush(stream);
+	else
+		return (-1);
+	input = 0;
+
+	buffer = malloc(sizeof(char) * 120);
+	if (!buffer)
+		return (-1);
+
+	while (c != '\n')
+	{
+		r = read(STDIN_FILENO, &c, 1);
+		if (r == -1 || (r == 0 && input == 0))
+			return (-1);
+		if (r == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+
+		if (input >= 120)
+			buffer = _realloc(buffer, input, input + 1);
+
+		buffer[input] = c;
+		input++;
+	}
+	buffer[input] = '\0';
+
+	assign_lineptr(lineptr, n, buffer, input);
+
+	ret = input;
+	if (r != 0)
+		input = 0;
+	return (ret);
 }
 
