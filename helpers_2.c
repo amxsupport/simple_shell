196b474d4a31149e98d20d0a305312d13d7e2dd1
amxsupport <itmasterdesigne@gmail.com>
2023-08-20 16:06:32 +0000

    create helpers_2.c file

---
+++ helpers_2.c
@@ -0,0 +1,40 @@
+/*
+ * File: helpers_2.c
+ * Auth: Alex Yu
+ *       Brennan D Baraban
+ */
+
+#include "shell.h"
+
+void handle_line(char **line, ssize_t read);
+ssize_t get_new_len(char *line);
+void logical_ops(char *line, ssize_t *new_len);
+
+/**
+ * handle_line - Partitions a line read from standard input as needed.
+ * @line: A pointer to a line read from standard input.
+ * @read: The length of line.
+ *
+ * Description: Spaces are inserted to separate ";", "||", and "&&".
+ *              Replaces "#" with '\0'.
+ */
+void handle_line(char **line, ssize_t read)
+{
+	/*implementation*/
+}
+
+/**
+ * get_new_len - Gets the new length of a line partitioned
+ *               by ";", "||", "&&&", or "#".
+ * @line: The line to check.
+ *
+ * Return: The new length of the line.
+ *
+ * Description: Cuts short lines containing '#' comments with '\0'.
+ */
+
+ssize_t get_new_len(char *line)
+{
+	 /*implementation*/
+}
+
