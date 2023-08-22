d3e80eed9a5b42c569e6be04b94d05fb563bcf19
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-22 01:01:09 +0100

    Update alias_builtins.c implement shellby_alias function

--- alias_builtins.c
+++ alias_builtins.c
@@ -20,7 +20,41 @@ void print_alias(alias_t *alias);
  */
 int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 {
-/* implement */
+	alias_t *temp = aliases;
+	int i, ret = 0;
+	char *value;
+
+	if (!args[0])
+	{
+		while (temp)
+		{
+			print_alias(temp);
+			temp = temp->next;
+		}
+		return (ret);
+	}
+	for (i = 0; args[i]; i++)
+	{
+		temp = aliases;
+		value = _strchr(args[i], '=');
+		if (!value)
+		{
+			while (temp)
+			{
+				if (_strcmp(args[i], temp->name) == 0)
+				{
+					print_alias(temp);
+					break;
+				}
+				temp = temp->next;
+			}
+			if (!temp)
+				ret = create_error(args + i, 1);
+		}
+		else
+			set_alias(args[i], value);
+	}
+	return (ret);
 }
 
 /**
