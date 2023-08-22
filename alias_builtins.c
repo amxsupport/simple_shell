495dd2b199d803f81870e84746673f0a3bfd6c6a
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-22 01:18:07 +0100

    Update alias_builtins.c implement replace_aliases function

--- alias_builtins.c
+++ alias_builtins.c
@@ -124,4 +124,34 @@ void print_alias(alias_t *alias)
  */
 char **replace_aliases(char **args)
 {
+	alias_t *temp;
+	int i;
+	char *new_value;
+
+	if (_strcmp(args[0], "alias") == 0)
+		return (args);
+	for (i = 0; args[i]; i++)
+	{
+		temp = aliases;
+		while (temp)
+		{
+			if (_strcmp(args[i], temp->name) == 0)
+			{
+				new_value = malloc(sizeof(char) * (_strlen(temp->value) + 1));
+				if (!new_value)
+				{
+					free_args(args, args);
+					return (NULL);
+				}
+				_strcpy(new_value, temp->value);
+				free(args[i]);
+				args[i] = new_value;
+				i--;
+				break;
+			}
+			temp = temp->next;
+		}
+	}
+
+	return (args);
 }
