08ef3a35a37ac4aa1ac2475c48d367ee8aac47f6
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-08-22 01:11:08 +0100

    Update alias_builtins.c set set_alias function

--- alias_builtins.c
+++ alias_builtins.c
@@ -65,7 +65,34 @@ int shellby_alias(char **args, char __attribute__((__unused__)) **front)
  */
 void set_alias(char *var_name, char *value)
 {
-/* implement */
+	alias_t *temp = aliases;
+	int len, j, k;
+	char *new_value;
+
+	*value = '\0';
+	value++;
+	len = _strlen(value) - _strspn(value, "'\"");
+	new_value = malloc(sizeof(char) * (len + 1));
+	if (!new_value)
+		return;
+	for (j = 0, k = 0; value[j]; j++)
+	{
+		if (value[j] != '\'' && value[j] != '"')
+			new_value[k++] = value[j];
+	}
+	new_value[k] = '\0';
+	while (temp)
+	{
+		if (_strcmp(var_name, temp->name) == 0)
+		{
+			free(temp->value);
+			temp->value = new_value;
+			break;
+		}
+		temp = temp->next;
+	}
+	if (!temp)
+		add_alias_end(&aliases, var_name, new_value);
 }
 
 /**
