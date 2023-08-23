6570694b17468558c95dcf13e159b79164e3d54c
amxsupport <itmasterdesigne@gmail.com>
2023-08-23 00:04:21 +0000

    update env_builtins.c implement shellby_env function

--- env_builtins.c
+++ env_builtins.c
@@ -22,6 +22,20 @@ int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
  */
 int shellby_env(char **args, char __attribute__((__unused__)) **front)
 {
+	int index;
+	char nc = '\n';
+
+	if (!environ)
+		return (-1);
+
+	for (index = 0; environ[index]; index++)
+	{
+		write(STDOUT_FILENO, environ[index], _strlen(environ[index]));
+		write(STDOUT_FILENO, &nc, 1);
+	}
+
+	(void)args;
+	return (0);
 }
 
 /**
